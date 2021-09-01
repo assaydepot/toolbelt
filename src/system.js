e3433ee44d0ce952cf478cb0b87727ba4dea25e7
Ron Ranauro <ron@inciteadvisors.com>
2021-09-01 13:37:17 -0400

    updated memory to return heapUsed by default

--- src/system.js
+++ src/system.js
@@ -1,12 +1,22 @@
 module.exports = {
 	
 	// Purpose: Compute the memory used by the application. Note: Works for Node.js
-	memory: function() {
-
+	memory: function(full) {
+    let mem;
+    
 		if (console && console.memory && console.memory.usedJSHeapSize) {
 			return console.memory.usedJSHeapSize/1024000;
 		}
 		if (typeof exports !== 'undefined') {
+      if (full) {
+        mem = process.memoryUsage();
+        return {
+          rss: mem.rss/1024000,
+          heapTotal: mem.heapTotal/1024000,
+          heapUsed: mem.heapUsed/1024000,
+          external: mem.external/1024000
+        }
+      }
 			return (require('util').inspect(process.memoryUsage().rss/1024000));
 		};
 	},
